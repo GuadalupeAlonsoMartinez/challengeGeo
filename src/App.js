8dd57b04cf5c8f5924321e038c9a24101fdc32dc
GuadalupeAlonsoMartinez <16161258@itoaxaca.edu.mx>
2022-10-27 22:11:11 -0500

    future(geolocalitation): first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [positionsState, setPositionsState] = useState([]);
+  const eventClick = function () {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      // Show a map centered at latitude / longitude.
+      saveCoords(latitude, longitude);
+    });
+  };
+  const saveCoords = function (latitude, longitude) {
+    var listPosition = localStorage.getItem("positions");
+    var positions = [];
+    if (listPosition !== null) {
+      positions = JSON.parse(listPosition);
+    }
+    positions.push({ latitude, longitude });
+    setPositionsState([positionsState,...positions]);
+    localStorage.setItem("positions", JSON.stringify(positions));
+  };
+  const createRows = function () {
+    return positionsState.map((position,index) => {
+      const { latitude, longitude } = position;
+      return (
+        <tr key={index}> 
+          <td>{latitude}</td>
+          <td>{longitude}</td>
+        </tr>
+      );
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={eventClick}>LOCATION NOW</button>
+      <table>
+        <thead>
+          <tr>
+            <th>latitude</th>
+            <th>longitude</th>
+          </tr>
+        </thead>
+        <tbody>{createRows()} </tbody>
+      </table>
     </div>
   );
 }
